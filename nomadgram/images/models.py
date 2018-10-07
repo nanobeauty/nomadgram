451f192bae577d039d892c8ee8f9784196fb509d
Nano_Macbook <nanobeauty@icloud.com>
2018-10-07 16:56:35 +0700

    < Creating the Like Model and fixing commnet and Image Models >
    ForeignKey 사용으로 연결.
    
    1 to many - many to 1 Model
    
    users/models.py 파일에
    
    “”” User Model “””
    
    추가하자
     images/models.py 파일의
    user_models.User
    부부의 User에 hover 시 링크된 부분이 나타난다.
    
    python manage.py makemigrations
    puthon manage.py migrate

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
 
@@ -9,11 +10,29 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+
 class Image(TimeStampedModel):
+
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
 
-class comment(TimeStampedModel):
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
     comment = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
 
+    
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
