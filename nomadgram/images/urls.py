69ff021630e56220f1e04e046cb56090d144327b
Nano_Macbook <nanobeauty@icloud.com>
2018-10-22 11:46:02 +0700

    <  Commenting on an image  >
    127.0.0.1:8000/images/14/like
    에서 포스트 버튼을 클릭하여
    {“message”:”Hello NO 14”}
    쿼리를 DB에 정장
    
    유저와 이미지를 연력하는 것, 시리얼라이저에서 데이터를 변환하고 cretor를 read_only로 하는 것이 중요
    
    http://127.0.0.1:8000/admin/images/comment/
    http://localhost:8000/images/
    
    위 두 url에서 확인 가능

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -5,6 +5,7 @@ app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     path("<image_id>/like", view=views.LikeImage.as_view(), name="like_image"),
+    path("<image_id>/comment", view=views.CommentOnImage.as_view(), name="comment_image"),
 ]
 
 
