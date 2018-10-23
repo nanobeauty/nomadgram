17d8fe4bd79b8d10fa50da80a7ff5a88105c3fb4
Nano_Macbook <nanobeauty@icloud.com>
2018-10-23 09:51:20 +0700

    <  Deleting my own comment  >
    http://127.0.0.1:8000/images/comments/3(comment_id)
    에서 삭제 버튼을 클릭하여 삭제
    
    http://127.0.0.1:8000/admin/images/comment/
    http://localhost:8000/images/
    
    위 두 url에서 확인 가능

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -4,16 +4,7 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<image_id>/like", view=views.LikeImage.as_view(), name="like_image"),
-    path("<image_id>/comment", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("<image_id>/likes", view=views.LikeImage.as_view(), name="like_image"),
+    path("<image_id>/comments", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<comment_id>", view=views.Comment.as_view(), name="comment"),
 ]
-
-
-
-# images/3/like
-# images/3/unlike
-# 
-# 1. create the url and the view
-# 2. take the id from the url
-# 3. We want to find an image with this id
-# 4. We want to create a like with that image
