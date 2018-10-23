a6148efdbea9126ff5603420036c89fe6c81bb3d
Nano_Macbook <nanobeauty@icloud.com>
2018-10-23 15:27:09 +0700

    <Following a User>

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -7,7 +7,8 @@ class ExploreUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = (
+            'id',
             'profile_image',
             'username',
-            'name'
+            'name',
         )
--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
+    path("user_id/follow", view=views.FollowUser.as_view(), name="follow_user"),
 ]
